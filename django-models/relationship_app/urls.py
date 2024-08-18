06ba2096d87cd7265fbbc81fa835ccda6fbe330e
zak mari <marioush741@gmail.com>
2024-08-18 22:55:32 +0100

    asdsd

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,16 +1,29 @@
-from atexit import register
-from . import views
-from .views import CustomLoginView, CustomLogoutView, list_books, LibraryDetailView
+from django.urls import path
+from .views import CustomLoginView, CustomLogoutView, register, list_books, LibraryDetailView
 
-from django.contrib import admin
-from django.urls import include, path
- 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relationship_app/', include('relationship_app.urls')),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
 ]
+
+
+
+# from atexit import register
+# from .import views
+# from .views import CustomLoginView, CustomLogoutView, register, list_books, LibraryDetailView
+
+# from django.contrib import admin
+# from django.urls import include, path
+ 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('relationship_app/', include('relationship_app.urls')),
+#     path('books/', list_books, name='list_books'),
+#     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+#     path('login/', CustomLoginView.as_view(), name='login'),
+#     path('logout/', CustomLogoutView.as_view(), name='logout'),
+#     path('register/', register, name='register'),
+# ]
