4d37bcf86095f46c0ff2722e051bd2fbd9f6711c
zak mari <marioush741@gmail.com>
2024-08-18 21:54:53 +0100

    asdsd

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from .models import Book
+from django.views.generic.detail import DetailView
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
-
-
 
 from django.views.generic import DetailView
 from .models import Library
