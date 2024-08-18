875ddb86c75b15459447839ccaf29d27dc45ad5c
zak mari <marioush741@gmail.com>
2024-08-18 22:02:20 +0100

    asdsd

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('relationship_app/', include('relationship_app.urls')),
+]
+
+
