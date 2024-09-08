64996ebf6d27c37f0bb57b973acbd2e5901f0177
zak mari <marioush741@gmail.com>
2024-09-08 19:37:24 +0100

    fsdg

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Created Author model
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title    
